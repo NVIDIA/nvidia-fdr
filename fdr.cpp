9b8740e1da8caccf88ab3dfd342ecef6270f820c
Yong (Leo) Li <yonl@nvidia.com>
2023-07-28 02:54:07 +0000

    build: migrate to meson

--- fdr.cpp
+++ fdr.cpp
@@ -11,7 +11,8 @@
 
 #include <filesystem>
 #include <sys/stat.h>
-#include <spdlog/spdlog.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
 #include "fdr.hpp"
 
 std::string bootCounter;
@@ -102,7 +103,7 @@ void FlightDataRecorder_c::InitLogger() {
 
 	std::vector<spdlog::sink_ptr> sinks;
 	// stdout logger
-	auto stdout_logger = std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>();
+	auto stdout_logger = std::make_shared<spdlog::sinks::stdout_sink_mt>();
 	sinks.push_back(stdout_logger);
 	// file logger
 	try {
--- tools/yaml-converter.py
+++ tools/yaml-converter.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+yaml-cpp is lacking anchor/alias support
+https://github.com/jbeder/yaml-cpp/issues/353
+
+This tools is created as a workaround, it will be no longer needed 
+once migrated to different yaml lib with proper anchor/alias support.
+"""
+
 import os
 import sys
 import yaml
@@ -24,14 +32,33 @@ class UniqueKeyLoader(yaml.SafeLoader):
         return super().construct_mapping(node, deep)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if not (2 <= len(sys.argv) <= 3):
         sys.stderr.write ("""
-            Usage: {} yaml-file
-            """.format(os.path.basename(sys.argv[0])))
+Usage: {} input.yaml [output.yaml]
+
+""".format(os.path.basename(sys.argv[0])))
         exit(1)
+
+    input_file = sys.argv[1]
+
+    if len(sys.argv) == 3:
+        output_file = sys.argv[2]
+        if os.path.abspath(input_file) == os.path.abspath(output_file):
+            sys.stderr.write ("""
+Input/Output are the same file.
+Please don't build in source tree, instead setup a separate build directory.
+
+    cmake -B build
+    make -C build
+
+""")
+            exit(1)
+        out = open(output_file, "w")
+    else:
+        out = sys.stdout
     
     yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, UniqueKeyLoader.construct_mapping)
-    with open(sys.argv[1], "r") as f:
+    with open(input_file, "r") as f:
         doc = yaml.load(f, Loader=UniqueKeyLoader)
 
-    print (yaml.dump(doc, indent=2, Dumper=NoAliasDumper))
+    out.write(yaml.dump(doc, indent=2, Dumper=NoAliasDumper))
