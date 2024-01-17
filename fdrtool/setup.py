2a7074909875f6b68eea66e8d43edd0f7b256787
shivamc <shivamc@cvam0000.nvidia.com>
2024-01-17 15:32:00 +0530

    Add setuptool to package fdrtool - Add pyinstaller for compilation of the fdrtool to the binaries elf files. - Add setup tools to package fdrtool.
    How to build
    :~/nvidia-fdr/fdrtool$ make install
    
    Where to find the package
    :~/nvidia-fdr/fdrtool$ cd dist/
    :~/nvidia-fdr/fdrtool/dist$ ls
    fdr_logs fdrtool fdrtool-1.0-py3-none-any.whl fdrtool-1.0.tar.gz
    
    How to install
    :~$ pip3 install fdrtool-1.0.tar.gz
    
    Info
    :~$ pip3 show fdrtool
    
    How to run
    :~$ fdrtool  -ul --local_file '/home/shivamc/FDR-Dump3-24hrCompact-MultiBootcount-similiarToFactory.tar' --json -ld -kn
    
    Testing :Locally done , alpha phase is going on
    
    FIXES JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-10392
    
    Signed-off-by: Shivam shivamc@nvidia.com

---
+++ fdrtool/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+import subprocess
+
+# Function to run PyInstaller
+def run_pyinstaller():
+    subprocess.run(['pyinstaller', '--onefile', 'fdrtool.py'])
+
+
+# Run PyInstaller and compile protobuf before building the package
+run_pyinstaller()
+
+# Define your package and dependencies
+setup(
+    name='fdrtool',
+    version='1.0',
+    packages=find_packages(),
+    package_data={'fdrtool': ['../fdr_logs_schema_pb2.py']},  # Include the generated Python file
+    install_requires=[
+        #dependencies here
+    ],
+    data_files=[('bin', ['dist/fdrtool'])],
+)
+
