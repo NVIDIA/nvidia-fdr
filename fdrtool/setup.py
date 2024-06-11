0b198ccc6410efa4bedeb46323621dd3aae95888
shivamc <shivamc@cvam0000.nvidia.com>
2024-06-11 12:19:06 +0530

    - Update fdrtool version to 1.1 - Update gitignore file to ignore few temporary files.
    FIXES JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-12547
    Signed-off-by: Shivam shivamc@nvidia.com

--- fdrtool/setup.py
+++ fdrtool/setup.py
@@ -13,7 +13,7 @@ run_pyinstaller()
 # Define your package and dependencies
 setup(
     name='fdrtool',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     package_data={'fdrtool': ['../fdr_logs_schema_pb2.py']},  # Include the generated Python file
     install_requires=[
